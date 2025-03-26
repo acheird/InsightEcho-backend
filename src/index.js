3dcb6fa8302df2834e19920502b7c79fbab6a7c6
acheird <acheird@gmail.com>
2025-03-26 09:43:58 +0200

    import and connect reviews routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (req,res) => {
-    res.send("Server is running");
-});
+// Import routes
+const reviewRoutes = require("./routes/reviews"); 
+// Connect routes
+app.use("/api", reviewRoutes); 
 
-const PORT = process.env.PORT || 5000;
+app.get("/ping", (req, res) => {
+    res.send("Server is running!");
+  });
+
+  const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+console.log(`Server running on port ${PORT}`);
 });
