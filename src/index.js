81be067235757b1ef0cfe8ff17b70ca8563fee13
acheird <acheird@gmail.com>
2025-03-27 08:00:11 +0200

    Add some Debugging console logs

--- src/index.js
+++ src/index.js
@@ -6,14 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// console.log("🔹 Trying to load routes...");
+
 // Import routes
 const reviewRoutes = require("./routes/reviews"); 
+
+//console.log("🔹 reviewRoutes:", reviewRoutes);
+
 // Connect routes
 app.use("/api", reviewRoutes); 
 
 app.get("/ping", (req, res) => {
     res.send("Server is running!");
   });
+
+ /*
+  app._router.stack.forEach((r) => {
+    if (r.route && r.route.path) {
+      console.log(`Route found: ${r.route.path}`);
+    }
+  });
+
+*/
 
   const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
