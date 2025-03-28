0d67deca4cfbbcb8029381c84a36654ebeb064eb
acheird <acheird@gmail.com>
2025-03-28 11:19:48 +0200

    add CleanText to remove any special characters from reviews text

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,6 +1,26 @@
 // Import database connection
 const pool = require("../db"); 
 
+/*
+const Sentiment = require("Sentiment");
+const sentiment = new Sentiment();
+const config = require("../config"); */
+
+// Stopwords
+const STOPWORDS = new Set([
+  "the", "is", "and", "or", "a", "an", "to", "in", "of", "for", "on", "with", "as"
+]);
+
+// Take review's text as input and clean it
+const cleanText = (text) => {
+  return text
+    .toLowerCase()
+    .replace(/[^a-zA-Z ]/gi, '') // Remove special characters
+    .split(" ")
+    .filter(word => !STOPWORDS.has(word)) // Remove stopwords
+    .join(" ");
+};
+
 // Function to add a review
 const addReview = async (req, res) => {
   const { text, rating } = req.body;
@@ -21,7 +41,8 @@ const addReview = async (req, res) => {
 };
 
 // Function to analyze reviews (placeholder)
-const getAnalysis = (req, res) => {
+const getAnalysis = async (req, res) => {
+
   res.json({ message: "Sentiment analysis not implemented yet." });
 };
 
