be1a99ee8d97b62b92975d2e23ca8707638b85a8
acheird <acheird@gmail.com>
2025-03-29 10:23:16 +0200

    adding some comments in index.js

--- src/index.js
+++ src/index.js
@@ -3,33 +3,30 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-// console.log("🔹 Trying to load routes...");
 
 // Import routes
 const reviewRoutes = require("./routes/reviews"); 
 
-//console.log("🔹 reviewRoutes:", reviewRoutes);
-
 // Connect routes
 app.use("/api", reviewRoutes); 
+
 
 app.get("/ping", (req, res) => {
     res.send("Server is running!");
   });
 
- /*
-  app._router.stack.forEach((r) => {
-    if (r.route && r.route.path) {
-      console.log(`Route found: ${r.route.path}`);
-    }
-  });
+// Debugging: Log available routes
+app._router.stack.forEach((r) => {
+  if (r.route && r.route.path) {
+    console.log(`Route found: ${r.route.path}`);
+  }
+});
 
-*/
-
-  const PORT = process.env.PORT || 5000;
+// Start Server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
